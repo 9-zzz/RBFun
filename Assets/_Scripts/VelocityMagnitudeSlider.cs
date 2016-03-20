657aec6f11a4b7e27744833f2c26ba5e178c4360
Shahan Akhter <shahan3.14@gmail.com>
2016-03-20 00:15:28 -0400

    added trails to bullet pieces, weird shadow glitch with lighting? normal bias? maybe plane mesh? not sure sucks.. added v magnitude slider

---
+++ Assets/_Scripts/VelocityMagnitudeSlider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VelocityMagnitudeSlider : MonoBehaviour {
+
+	Slider vSlider;
+    void Awake()
+    {
+        vSlider = GetComponent<Slider>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vSlider.value = Mathf.MoveTowards(vSlider.value,RBFPS.S.rb.velocity.magnitude,Time.deltaTime*5.0f);
+    }
+}
